e5dc6bce041df3007a0fc264c3b9a67f9f094556
JC Kolodziej <kuba@kolodziej.com.pl>
2019-05-14 11:42:04 +0200

    possible solution for next/previous song

--- funcs.py
+++ funcs.py
@@ -54,20 +54,38 @@ def volume():
 def get_songs_from_dir(directory):
     import os
     songs = []
-
-    # r=root, d=directories, f = files
-    for r, d, f in os.walk(directory):
-        for file in f:
-            if '.mp3' in file:
-                songs.append(os.path.join(r, file))
+    if '.mp3' in directory:
+        songs.append(directory)
+    else:
+        # r=root, d=directories, f = files
+        for r, d, f in os.walk(directory):
+            for file in f:
+                if '.mp3' in file:
+                    songs.append(os.path.join(r, file))
     return songs
 
 def play_playlist(songs):
-    for song in songs:
-        play_song(song)
+    for i in range(len(songs)):
+        play_song(songs[i])
+        '''
+        if event = next song
+            stop song
+            i += 1
+        elif event = previous song
+            stop song
+            i -= 1 or 2 -> needs testing
+        '''
+
+def shuffle(songs):
+    import random
+    random.shuffle(songs)
+    print(songs)
+    return songs
 
 
 #print(get_songs_from_dir('/home/jck/Documents/python/dizzydeer/DizzyDeer/songs/'))
 #play_song(get_songs_from_dir('/home/jck//Documents/python/dizzydeer/DizzyDeer/songs')[0])
 
-play_playlist(get_songs_from_dir('/home/jck/Documents/python/dizzydeer/DizzyDeer/songs/'))
+
+play_playlist(shuffle(get_songs_from_dir('/home/jck/Documents/python/dizzydeer/DizzyDeer/songs/')))
+
