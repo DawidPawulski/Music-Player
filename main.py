71666218ea3e375429bbf5dd3cb362ff479486b1
JC Kolodziej <kuba@kolodziej.com.pl>
2019-05-14 13:35:36 +0200

    start GUI

---
+++ main.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+
+window = Tk()
+window.title('Dizzy Dear')
+#window.iconbitmap('dizzydeer.ico')
+
+
+
+if __name__ == '__main__':
+    mainloop()
--- window.py
+++ window.py
@@ -1,8 +1,10 @@
 from tkinter import *
 
 class DeezyDeer():
-
-
-window = Tk()
+    window = Tk()
 app = DeezyDeer(master = window)
 app.mainloop()
+
+
+p = ttk.Progressbar(parent, orient=HORIZONTAL, length=200, mode='determinate') # progress bar -> uncklickable
+s = ttk.Scale(parent, orient=HORIZONTAL, length=200, from_=1.0, to=100.0) # scale for volume
