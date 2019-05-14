92419fdd758d9f895f78f541cb18859b506426c8
JC Kolodziej <kuba@kolodziej.com.pl>
2019-05-14 14:42:16 +0200

    more buttons and more songs

--- window.py
+++ window.py
@@ -1,7 +1,10 @@
 from tkinter import *
+from tkinter import filedialog
 import funcs
 
-
+def pick_dir():
+    directory = filedialog.askdirectory()
+    return directory
 def window():
 
     window = Tk()
@@ -13,9 +16,23 @@ def window():
     #window.iconbitmap(r'/home/jck/Documents/python/dizzydeer/DizzyDeer/dizzydeer.ico')
 
 
-    button_play = Button(window, text = 'PLAY', command = lambda: funcs.play_playlist(funcs.get_songs_from_dir('/home/jck/Documents/python/dizzydeer/DizzyDeer/songs/')))
-    button_play.pack()
+    # filedialog.askdirectory()
     
+    #window.file =  filedialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("mp3 files","*.mp3"),("all files","*.*")))
+    directory = pick_dir()
+
+
+    button_play = Button(window, text = 'PLAY', command = lambda: funcs.play_playlist(funcs.get_songs_from_dir(directory)))
+    button_play.pack()
+    button_stop = Button(window, text = 'STOP', command = lambda: funcs.stop_song())
+    button_stop.pack()
+    button_pick = Button(window, text = 'Pick Directory', command = lambda: pick_dir())
+    button_pick.pack()
+
+
+
+
+
 
 
     while True: # placeholder
