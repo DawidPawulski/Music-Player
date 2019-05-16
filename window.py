911d019088137bcf4ef38cf49cf34d2011b4d5b9
JC Kolodziej <kuba@kolodziej.com.pl>
2019-05-16 13:11:07 +0200

    minor adjustments and bug fixes

--- window.py
+++ window.py
@@ -4,12 +4,12 @@ from tkinter import ttk
 import funcs
 import pygame
 
+
 def pick_dir():
     directory = filedialog.askdirectory()
     return directory
 
 def window():
-
 	window = Tk()
 	window.title('Dizzy Dear')
 	window.geometry('450x100')
@@ -19,29 +19,14 @@ def window():
 	topFrame=Frame(window)
 	topFrame.pack()
 
-
-    #window.iconbitmap(r'/home/jck/Documents/python/dizzydeer/DizzyDeer/dizzydeer.ico')
-
-    #label_smth = Label(window, text = "  ")    
-
-    # filedialog.askdirectory()
-    
-    #window.file =  filedialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("mp3 files","*.mp3"),("all files","*.*")))
 	directory = pick_dir()
-
 
 	bottomFrame = Frame(window)
 	bottomFrame.pack(side = BOTTOM)
 
-
-
-
 	pb = ttk.Progressbar(window, orient="horizontal", length=400, mode="determinate")
 	pb.pack()
 	
-
-
-
 	button_previous_song = Button(topFrame, text = '|<<',activebackground = "purple", command = lambda: funcs.previous_song(funcs.get_songs_from_dir(directory)))
 	button_previous_song.pack(side=LEFT)
 	button_play = Button(topFrame, text = '| >',activebackground = "purple", command = lambda: funcs.play_playlist(funcs.get_songs_from_dir(directory), song_index))
@@ -50,15 +35,12 @@ def window():
 	button_stop.pack(side=LEFT)
 	button_next_song = Button(topFrame, text = '>>|',activebackground = "purple", command = lambda: funcs.next_song(funcs.get_songs_from_dir(directory)))
 	button_next_song.pack(side=LEFT)
-	button_pick = Button(window, text = 'PD',activebackground = "purple", command = lambda: pick_dir())
-	button_pick.pack(side=RIGHT,fill = Y)
+	#button_pick = Button(window, text = 'PD',activebackground = "purple", command = lambda: pick_dir())
+	#button_pick.pack(side=RIGHT,fill = Y)
+	slider_volume = Scale(window, from_=0, to=100, orient=HORIZONTAL)
+	slider_volume.pack(side=RIGHT)
 	pb.start()
 	
-
-
-
-
-
 
 	while True: # placeholder
 		mainloop()
